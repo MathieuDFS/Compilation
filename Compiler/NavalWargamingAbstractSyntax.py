0e9d25bbb7b0198025bdf2a3b96727cd264fc6a3
Mathieu_D <mathieu.duflos@hotmail.fr>
2023-04-30 11:08:52 +0200

    Update Abstract Syntax to accept a Visitor

--- Compiler/NavalWargamingAbstractSyntax.py
+++ Compiler/NavalWargamingAbstractSyntax.py
@@ -5,7 +5,12 @@ class Abstract_Syntax_Tree():
 
 class AST_Node():
     """Abstract class for AST nodes"""
-    pass
+    def __init__(self):
+        pass
+
+    def accept(self, visitor):
+        """Accept a visitor (pattern visitor)"""
+        pass
 
 
 
@@ -13,6 +18,9 @@ class Program(AST_Node):
     def __init__(self,initial_State, actions):
         self.initial_State: Initial_State = initial_State
         self.actions: list[Action] = actions
+
+    def accept(self, visitor):
+        visitor.visitProgram(self)
 
 
 class Initial_State(AST_Node):
@@ -22,28 +30,48 @@ class Initial_State(AST_Node):
         self.fleets: Fleets = fleets
         self.map: Map = map
 
+    def accept(self, visitor):
+        visitor.visitInitial_State(self)
+
 class Action(AST_Node):
-    pass #todo
+    pass # TODO
 
 class Factions(AST_Node):
     def __init__(self, faction_list):
         self.faction_list: list[Faction] = faction_list
+
+    def accept(self, visitor):
+        visitor.visitFactions(self)
+
 class Faction(AST_Node):
     def __init__(self, identifier):
         self.identifier: Identifier = identifier
 
+    def accept(self, visitor):
+        visitor.visitFaction(self)
+
 class Relations:
     def __init__(self, relation_list):
         self.relation_list: list[Relation] = relation_list
+
+    def accept(self, visitor):
+        visitor.visitRelations(self)
+
 class Relation(AST_Node):
     def __init__(self, faction1, faction2, relation):
         self.faction1: str = faction1
         self.faction2: str = faction2
         self.relation: int = relation
 
+    def accept(self, visitor):
+        visitor.visitRelation(self)
+
 class Fleets(AST_Node):
     def __init__(self, faction_fleet):
         self.faction_fleet: list[Faction_Fleet] = faction_fleet
+
+    def accept(self, visitor):
+        visitor.visitFleets(self)
 
 class Faction_Fleet(AST_Node):
     def __init__(self, faction,flotilla_list):
@@ -55,20 +83,36 @@ class Flotilla(AST_Node):
         self.identifier: Identifier = identifier
         self.vessels: list[Vessel] = vessels
 
+    def accept(self, visitor):
+        visitor.visitFlotilla(self)
+
 class Vessel(AST_Node):
     def __init__(self, number, vessel_type):
         self.number: int = number
         self.vessel_type: Vessel_type = vessel_type
 
+    def accept(self, visitor):
+        visitor.visitVessel(self)
+
 class Vessel_type(AST_Node):
     def __init__(self, type):
         self.type:str = type
+
+    def accept(self, visitor):
+        visitor.visitVessel_type(self)
+
 
 class Map(AST_Node):
     def __init__(self, x,y):
         self.x: int = x
         self.y: int = y
 
+    def accept(self, visitor):
+        visitor.visitMap(self)
+
 class Identifier(AST_Node):
     def __init__(self, name):
         self.name: str = name
+
+    def accept(self, visitor):
+        visitor.visitIdentifier(self)
