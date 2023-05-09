59c07d06de2c02d4c8e8193102d81e6113247f08
Mathieu_D <mathieu.duflos@hotmail.fr>
2023-05-09 17:46:52 +0200

    Update ReadMe and allow IDE run

--- NavalWargaming/main.py
+++ NavalWargaming/main.py
@@ -6,17 +6,24 @@ from Compiler.PrettyPrinter import PrettyPrinter
 from Compiler.SemanticVisitor import SemanticVisitor
 
 if __name__ == "__main__":
-    PrettyPrint =False
-    if(len(sys.argv) == 3):
-        PrettyPrint = True
-        prettyPrintName = sys.argv[2]
-    elif (len(sys.argv) != 2):
-        print("Usage: python main.py <file_name>")
-        print("Or: python main.py <file_name> <pretty_print_name>")
-        print("Example: python main.py ../examples/unit_exemple3.nwg")
-        exit(1)
+    IDE = False # Set to True if you want to run the program in the IDE
+    PrettyPrint = False
 
-    filename = sys.argv[1]
+    if IDE:
+        filename = "../examples/unit_exemple3.nwg" # Change this line to run another file
+        PrettyPrint = False # Set to True if you want to pretty print the AST
+        prettyPrintName ="exemple3" # Change this line to change the name of the pretty printed file
+    else:
+        if(len(sys.argv) == 3):
+            PrettyPrint = True
+            prettyPrintName = sys.argv[2]
+        elif (len(sys.argv) != 2):
+            print("Usage: python main.py <file_name>")
+            print("Or: python main.py <file_name> <pretty_print_name>")
+            print("Example: python main.py ../examples/unit_exemple3.nwg")
+            exit(1)
+
+        filename = sys.argv[1]
 
     L = NavalWargamingLexer()
     parser = NavalWargamingParser(L.lex_file(filename))
