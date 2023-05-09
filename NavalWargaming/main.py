59d0ced376be517a16feb4f2346c6d696617d3b7
Mathieu_D <mathieu.duflos@hotmail.fr>
2023-05-09 17:53:10 +0200

    Minor change

--- NavalWargaming/main.py
+++ NavalWargaming/main.py
@@ -6,18 +6,18 @@ from Compiler.PrettyPrinter import PrettyPrinter
 from Compiler.SemanticVisitor import SemanticVisitor
 
 if __name__ == "__main__":
-    IDE = False # Set to True if you want to run the program in the IDE
+    IDE = False  # Set to True if you want to run the program in the IDE
     PrettyPrint = False
 
     if IDE:
-        filename = "../examples/unit_exemple3.nwg" # Change this line to run another file
-        PrettyPrint = False # Set to True if you want to pretty print the AST
-        prettyPrintName ="exemple3" # Change this line to change the name of the pretty printed file
+        filename = "../examples/unit_exemple3.nwg"  # Change this line to run another file
+        PrettyPrint = False  # Set to True if you want to pretty print the AST
+        prettyPrintName = "exemple3"  # Change this line to change the name of the pretty printed file
     else:
-        if(len(sys.argv) == 3):
+        if len(sys.argv) == 3:
             PrettyPrint = True
             prettyPrintName = sys.argv[2]
-        elif (len(sys.argv) != 2):
+        elif len(sys.argv) != 2:
             print("Usage: python main.py <file_name>")
             print("Or: python main.py <file_name> <pretty_print_name>")
             print("Example: python main.py ../examples/unit_exemple3.nwg")
@@ -32,11 +32,8 @@ if __name__ == "__main__":
     semanticVisitor.visit(AST)
     wargame = semanticVisitor.InitializeWargame()
 
-    if (PrettyPrint):
+    if PrettyPrint:
         PrettyPrint = PrettyPrinter()
-        PrettyPrint.visit(AST,prettyPrintName)
+        PrettyPrint.visit(AST, prettyPrintName)
 
     wargame.run()
-
-
-
